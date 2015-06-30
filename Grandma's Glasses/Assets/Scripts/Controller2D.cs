c7081d1ad17908fccaf1a52c6c94283b47bd9c37
Andrew <clinkmeister@gmail.com>
2015-06-30 18:11:52 -0500

    Create initial project.

---
+++ Grandma's Glasses/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    BoxCollider2D collider;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
