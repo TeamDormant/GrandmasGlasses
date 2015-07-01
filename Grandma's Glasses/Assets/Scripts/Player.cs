4034c531a9dd70a04503935ec53fd57d73ec73a0
Andrew <clinkmeister@gmail.com>
2015-07-01 02:27:45 -0500

    Implemented Jumping, gravity control, velocity smoothing.

--- Grandma's Glasses/Assets/Scripts/Player.cs
+++ Grandma's Glasses/Assets/Scripts/Player.cs
@@ -4,17 +4,23 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
-    float jumpHeight = 4;
-    float timeToJumpApex = .4f;
-
-    float jumpVelocity = 8;
-    float moveSpeed;
+    public float jumpHeight = 3.5f;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float jumpVelocity;
+    float moveSpeed = 8;
     float gravity;
     Vector3 velocity;
+    float velocityXSmoothing;
     Controller2D controller;
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        print("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
 	}
 	
 	// Update is called once per frame
@@ -30,7 +36,8 @@ public class Player : MonoBehaviour {
         {
             velocity.y = jumpVelocity;
         }
-        velocity.x = input.x * moveSpeed;
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 	}
